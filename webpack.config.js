93c6305c09a560a30dbcd6f5d40262a49e2cee96
isha-san <isha.sangani@outlook.com>
2024-01-23 11:30:16 -0500

    remove webpack

--- webpack.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    plugins: [
-        new webpack.DefinePlugin({
-          'process.env.REACT_APP_CLIENT_ID': JSON.stringify(process.env.REACT_APP_CLIENT_ID),
-        })
-    ],
-    }
