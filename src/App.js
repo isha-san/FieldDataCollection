82d3002f59ff4bfe2d464347ecf4de7376ca002a
isha-san <isha.sangani@outlook.com>
2024-01-24 11:32:08 -0500

    install, require dotenv

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import CollectionForm from './components/CollectionForm.js';
 import EntryForm from './components/EntryForm.js';
 import Button from '@mui/material/Button';
 import Box from '@mui/system/Box';
+require('dotenv').config();
 
 export default function App() {
   const tokenClient = window.tokenClient;
